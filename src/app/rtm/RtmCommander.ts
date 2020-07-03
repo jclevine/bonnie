cf1d9b744342341686681c749343f56c11f69ee3
Justin Levine <justin_levine@ultimatesoftware.com>
2020-07-03 17:54:46 -0400

    Getting frob works

--- src/app/rtm/RtmCommander.ts
+++ src/app/rtm/RtmCommander.ts
@@ -1,6 +1,41 @@
 import { Task } from './Task';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FrobResponse } from './RtmResponse';
+
+export enum RtmPermissions {
+  READ = 'read',
+  WRITE = 'write',
+  DELETE = 'delete'
+}
+
+export interface RtmApiId {
+  apiKey: string;
+  secretKey: string;
+  rtmPermissions: RtmPermissions;
+}
 
 export class RtmCommander {
+
+  constructor(rtmApiId: RtmApiId, http: HttpClient) {
+    this.rtmApiId = rtmApiId;
+    this.http = http;
+  }
+  private rtmApiId: RtmApiId;
+  private http: HttpClient;
+
+  private static signRequest() {
+    return '2ab2e6ce5f3e008e04bfbd76195c2b4d';
+  }
+
+  private buildUrl(method: string): string {
+    return `/services/rest/?method=${method}&api_key=${this.rtmApiId.apiKey}&format=json&api_sig=${RtmCommander.signRequest()}`;
+  }
+
+  doAuth(): Observable<FrobResponse> {
+    return this.http.get<FrobResponse>(this.buildUrl('rtm.auth.getFrob'));
+  }
+
   getTasks(): Task[] {
     return [
       {
--- src/app/rtm/rtm.component.ts
+++ src/app/rtm/rtm.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { Permissions, RtmService } from './rtm.service';
+import { RtmService } from './rtm.service';
 import { Task } from './Task';
+import { RtmCommander, RtmPermissions } from './RtmCommander';
 
 @Component({
   selector: 'bo-rtm',
   templateUrl: './rtm.component.html',
-  styleUrls: ['./rtm.component.css']
+  styleUrls: ['./rtm.component.css'],
+  providers: [ RtmService ]
 })
 export class RtmComponent implements OnInit {
   tasks: Task[];
+  rtmCommander: RtmCommander;
 
   constructor(private rtmService: RtmService) {
-    this.tasks = rtmService.authenticate('blah', Permissions.WRITE).getTasks();
   }
 
   ngOnInit(): void {
+    this.rtmService.authenticate();
   }
 
 }
