7f51bdd16e6fbf6c8234085c798becefdd66f240
Justin Levine <justin_levine@ultimatesoftware.com>
2020-07-18 14:53:01 -0400

    Print out finish auth in console

--- src/app/rtm/rtm-commander.ts
+++ src/app/rtm/rtm-commander.ts
@@ -1,7 +1,9 @@
 import { Task } from './task';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { FrobResponse, RawFrobResponse } from './responses/rtm-response';
+import { concatMap } from 'rxjs/operators';
+import { SignatureInterceptor } from './signature-interceptor';
 
 export enum RtmPermissions {
   READ = 'read',
@@ -37,10 +39,37 @@ export class RtmCommander {
     };
   }
 
-  doAuth(): Observable<RawFrobResponse> {
+  doAuth(): Observable<string> {
     return this.http.get<RawFrobResponse>(
       '/services/rest/',
-      this.buildRequest('rtm.auth.getFrob'));
+      this.buildRequest('rtm.auth.getFrob')
+    ).pipe(
+      concatMap(response => of(this.printOutAcceptUrl(response)))
+    );
+  }
+
+  printOutAcceptUrl(rawFrobResponse: RawFrobResponse): string {
+    const url = this.buildAcceptUrl(new FrobResponse(rawFrobResponse).getFrob());
+    console.log(url);
+    return url;
+  }
+
+  buildAcceptUrl(frob: string) {
+
+    const signature = SignatureInterceptor.sign(this.rtmApiId.secretKey, new HttpParams({
+      fromObject: {
+        api_key: this.rtmApiId.apiKey,
+        perms: this.rtmApiId.rtmPermissions,
+        frob
+      }
+    }));
+    const url = new URL('https://www.rememberthemilk.com/services/auth/');
+    url.searchParams.append('api_key', this.rtmApiId.apiKey);
+    url.searchParams.append('perms', this.rtmApiId.rtmPermissions);
+    url.searchParams.append('frob', frob);
+    url.searchParams.append('api_sig', signature);
+    console.log(url.toString());
+    return url.toString();
   }
 
   getTasks(): Task[] {
--- src/app/rtm/signature-interceptor.ts
+++ src/app/rtm/signature-interceptor.ts
@@ -9,20 +9,11 @@ import { Md5 } from 'ts-md5';
 @Injectable()
 export class SignatureInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const signedReq = req.clone({
-      setParams: {
-        api_sig: this.sign(req.headers.get('secretKey'), req.params)
-      }
-    });
-    return next.handle(signedReq);
-  }
-
-  sign(secretKey: string, params: HttpParams): string {
+  static sign(secretKey: string, params: HttpParams): string {
     return String(new Md5().appendStr(`${secretKey}${this.buildSortedParamString(params)}`).end());
   }
 
-  private buildSortedParamString(params: HttpParams) {
+  private static buildSortedParamString(params: HttpParams) {
     const keys: string[] = params.keys().sort();
     let paramString = '';
     if (keys.length > 0) {
@@ -31,5 +22,14 @@ export class SignatureInterceptor implements HttpInterceptor {
     return keys.slice(1).reduce(
         (previousValue, currentValue) => previousValue.concat(currentValue, params.get(currentValue)),
         paramString);
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const signedReq = req.clone({
+      setParams: {
+        api_sig: SignatureInterceptor.sign(req.headers.get('secretKey'), req.params)
+      }
+    });
+    return next.handle(signedReq);
   }
 }
